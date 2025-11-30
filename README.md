4b1af7b8cd9af47b11ee4f5ee25db09b6fcee4b6
Guilherme Lopes de Oliveira <gui.lopes11@gmail.com>
2025-11-30 00:57:35 +0000

    feat: stripts  e  | configurações do Prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto ponta a ponta do tabnews.com.br (Trabalho do curso.dev)
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Comece antes de estar pronto!</h1>
+  return <h1>Comece antes de estar pronto!</h1>;
 }
 
 export default Home;
