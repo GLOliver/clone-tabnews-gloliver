46f3d0e5c4af1ea9cee981bcff4f71808f21f316
Guilherme Lopes de Oliveira <gui.lopes11@gmail.com>
2025-12-06 02:32:58 +0000

    feat: Finalizando o entpoint `/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/vi/status should return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const resBody = await res.json();
+  console.log(resBody);
+
+  const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
+  //expect(resBody.updated_at).toBe(parsedUpdatedAt);
+  expect(resBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(resBody.dependencies.database.version).toEqual("16.11");
+  expect(resBody.dependencies.database.max_connections).toEqual(100);
+  expect(resBody.dependencies.database.opened_connections).toEqual(1);
 });
